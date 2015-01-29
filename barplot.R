da137dcf234acb54e8f407901f0f229d48d6cba5
Shaofeng Duan <duansf@ihep.ac.cn>
2015-01-29 14:42:54 +0800

    replot the image

--- barplot.R
+++ barplot.R
@@ -2,18 +2,16 @@ library(ggplot2)
 
 load('barplot.RData')
 
-barplot_1 <-  ggplot(data=control_data,aes(x = order,y = control))
+barplot_1 <-  ggplot(data=data_control,aes(x = order,y = value))
 cols <- c("0" = "#808080","1" = "#22DDDD","2" = "#0909F7","3" = "#FF0000")
-barplot_1 + geom_bar(stat='identity',aes(fill = group)) + 
-coord_flip()+scale_fill_manual(values = cols,breaks = c("0","1","2"))
-
-barplot_1 <- barplot_1 + geom_bar(stat='identity',aes(fill = factor(con_col)),width = 0.8) + coord_flip()+scale_fill_manual(values = cols,
-breaks = c("0","1","2")) + xlab(' ') + ylab(' ') + ylim(0,7) + theme(axis.text.y = element_text(size=12))+theme(axis.text = element_text(colour = '#000000'))
 
 
+barplot_1 <- barplot_1 + geom_bar(stat='identity',aes(fill = factor(color)),width = 0.8) + coord_flip()+scale_fill_manual(values = cols,
+breaks = c("0","1","2")) + xlab(' ') + ylab(' ') + ylim(0,6) + theme(axis.text.y = element_text(size=12))+theme(axis.text = element_text(colour = '#000000'))
 
-barplot_2 <-  ggplot(data=occult_data,aes(x = order,y = occult))
-barplot_2 + geom_bar(stat='identity',aes(fill = group)) + 
-coord_flip()+scale_fill_manual(values = cols,breaks = c("0","1","2"))
 
-barplot_2 <- barplot_2 + geom_bar(stat='identity',aes(fill = factor(occ_col)),width = 0.8) + coord_flip()+scale_fill_manual(values = cols,breaks = c("0","1","3")) + xlab(' ') + ylab(' ') + ylim(0,7) + theme(axis.text.y = element_text(size=12))+theme(axis.text = element_text(colour = '#000000'))
+
+barplot_2 <-  ggplot(data=data_occult,aes(x = order,y = value))
+
+barplot_2 <- barplot_2 + geom_bar(stat='identity',aes(fill = factor(color)),width = 0.8) + coord_flip()+scale_fill_manual(values = cols,
+breaks = c("0","1","3")) + xlab(' ') + ylab(' ') + ylim(0,6) + theme(axis.text.y = element_text(size=12))+theme(axis.text = element_text(colour = '#000000'))
--- pointplot.R
+++ pointplot.R
@@ -4,11 +4,11 @@ load('point_data.RData')
 
 cols <- c("0" = "#808080","1" = "#22DDDD","2" = "#0909F7","3" = "#FF0000")
 
-fit_1 <- lm(occult~control,data=point_data)
-fitdata_1 <- data.frame(control = point_data$control,occult= fitted(fit_1))
+fit_1 <- lm(occult~control,data=mydata1)
+fitdata_1 <- data.frame(control = mydata1$control,occult= fitted(fit_1))
 
-pointplot_1 <- ggplot(data=point_data,aes(x=control,y=occult))
+pointplot_1 <- ggplot(data=mydata1,aes(x=control,y=occult))
 
 pointplot_1 <- pointplot_1 + geom_point(size = 4,shape = 19,aes(colour=factor(color))) + 
-geom_line(data=fitdata_1,linetype=2,size=2,colour = 'green')+scale_color_manual(values=cols,breaks=c("0","1","2","3"))+xlim(0,7) + ylim(0,7)
+geom_line(data=fitdata_1,linetype=2,size=2,colour = 'green')+scale_color_manual(values=cols,breaks=c("0","1","2","3"))+xlim(0,6) + ylim(0,6)
 
